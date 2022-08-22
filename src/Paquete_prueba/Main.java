087ee267f4982060f9ba9e755610b28065848c10
luigi-coder <luigicoder28@gmail.com>
2022-08-22 19:38:26 -0300

    Abstraccion, Herencia, Polimorfismo e Interfaces

--- src/Paquete_prueba/Main.java
+++ src/Paquete_prueba/Main.java
@@ -1,83 +1,94 @@
 package Paquete_prueba;
 
 public class Main {
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
 
-        /*
+    }
+}
+
+interface Vehiculo {
+
+    void Acelerar(int cuantaVelocidad);
+    void Frenar(int cuantaVelocidad);
+}
+
+class Cochecito implements Vehiculo {
+
+    @Override
+    public void Acelerar(int cuantaVelocidad) {
+
+    }
+
+    @Override
+    public void Frenar(int cuantaVelocidad) {
+
+    }
+}
+
+class Moto implements Vehiculo {
+
+    @Override
+    public void Acelerar(int cuantaVelocidad) {
+
+    }
+
+    @Override
+    public void Frenar(int cuantaVelocidad) {
+
+    }
+}
+
+/*
+public class Main {
+
+    public static void main(String[] args) {
+
         Cochecito coche = new Cochecito();
-        System.out.println(coche.numeroPuertas);
-        System.out.println(coche.velocidadMaxima);
-        System.out.println(coche.velocidadActual);
-
-        Cochecito coche2 = new Cochecito(2, 90);
-        System.out.println(coche2.numeroPuertas);
-        System.out.println(coche2.velocidadMaxima);
-        System.out.println(coche2.velocidadActual);
-        */
-
-        /*
-        Vehiculo auto = new Vehiculo();
-        auto.setTipo("Coupe");
-        auto.setVelocidadMaxima(120);
-        //auto.setRapido(true);
-        System.out.println("TIPO VEHICULO: " + auto.getTipo());
-        System.out.println("VELOCIDAD MAXIMA: " + auto.getVelocidadMaxima());
-        //System.out.println("RAPIDO: " + auto.isRapido());
-
-        Vehiculo moto = new Vehiculo();
-        moto.setTipo("Scotter");
-        moto.setVelocidadMaxima(50);
-        //moto.setRapido(false);
-        System.out.println("TIPO VEHICULO: " + moto.getTipo());
-        System.out.println("VELOCIDAD MAXIMA: " + moto.getVelocidadMaxima());
-        //System.out.println("RAPIDO: " + moto.isRapido());
-        */
-
+        coche.setSonido("BRRR");
+        System.out.println(coche.getSonido());
     }
 }
 
-class Cochecito {
-
-    int numeroPuertas;
-    int velocidadMaxima;
-    float velocidadActual;
-
-    // Sobrecarga de constructores.
-    public Cochecito(){
-        numeroPuertas = 5;
-        velocidadMaxima = 120;
-        System.out.println("Estoy en el constructor...");
-    }
-    public Cochecito(int numeroPuertas, int velocidadMaxima){
-        this.numeroPuertas = numeroPuertas;
-        this.velocidadMaxima = velocidadMaxima;
-        System.out.println("Estoy en el constructor con parametros...");
-    }
-}
-
+// HERENCIA Y ABSTRACCION
 abstract class Vehiculo {
+    int velocidadMaxima;
+    String matricula;
+    String sonido;
 
-    private String tipo;
-    private int velocidadMaxima;
-    private String sonido;
-
-    public String getTipo(){
-        return tipo;
-    }
-    public void setTipo(String tipo){
-        this.tipo = tipo;
+    public Vehiculo() {
+        System.out.println("Estoy en el constructor de Vehiculo");
     }
 
-    public int getVelocidadMaxima() {
-        return velocidadMaxima;
-    }
-
-    public void setVelocidadMaxima(int velocidadMaxima) {
-        this.velocidadMaxima = velocidadMaxima;
-    }
-
-    abstract String isSonido();
+    abstract String getSonido();
 
     abstract public void setSonido(String sonido);
 }
+
+class Cochecito extends Vehiculo{
+
+    @Override
+    String getSonido() {
+        return "Soy un super sonido de coche " + this.sonido;
+    }
+
+    @Override
+    public void setSonido(String sonido) {
+        this.sonido = sonido;
+    }
+}
+
+class Moto extends Vehiculo{
+
+    @Override
+    String getSonido() {
+        return "Soy un super sonido de moto " + this.sonido;
+    }
+
+    @Override
+    public void setSonido(String sonido) {
+        this.sonido = sonido;
+    }
+}
+
+*/
